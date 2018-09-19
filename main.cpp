e75d0f6a2f99a5862dc8c5b5beda2fb46781d182
Hill <ehill27@uncc.edu>
2018-09-18 21:21:35 -0400

    tweaking

--- main.cpp
+++ main.cpp
@@ -27,7 +27,7 @@ Adafruit_SSD1306 display(OLED_RESET);
 
 #define NUM_SAMPLES 10                // number of samples taken of the battery voltage measurement
 #define VoltageDivider1 11.15         // calibration factor for RX unit
-#define VoltageDivider2 10.10        // calibration factor for TX unit
+#define VoltageDivider2 9.45          // calibration factor for TX unit
 #define timeDelay 1000                // delay in ms to when the voltage is measured when load is applied, 4 sec
 #define switchPin 6                   // mode switch input pin
 #define RX 0                          // RX mode
